e3598fb38f0239f064d4219f58a192f2fa963c0a
richLSb <rigorous@live.co.uk>
2021-04-07 12:13:24 +0100

    userstory1_unfinished

--- spec/features/test_spec.rb
+++ spec/features/test_spec.rb
@@ -1,7 +1,7 @@
 feature 'test' do
- scenario 'it returns a test page' do 
+ scenario 'it returns a test page' do
   visit('/test')
 
   expect(page).to have_content('This is a test page!')
- end 
-end 
+ end
+end
