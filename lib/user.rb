a3de85a81f64dfe1a2d3264e6fb184d386280cfc
richLSb <rigorous@live.co.uk>
2021-04-08 11:55:01 +0100

    removed name from sel.create

--- lib/user.rb
+++ lib/user.rb
@@ -11,7 +11,7 @@ class User
 
   def self.create(email:, password:)
     encrypted_password = BCrypt::Password.create(password)
-    result = DatabaseConnection.query("INSERT INTO users (email, password) VALUES('#{email}', '#{encrypted_password}') RETURNING id, name, email;")
+    result = DatabaseConnection.query("INSERT INTO users (email, password) VALUES('#{email}', '#{encrypted_password}') RETURNING id, email;")
     User.new(
       id: result[0]['id'],
       email: result[0]['email']
